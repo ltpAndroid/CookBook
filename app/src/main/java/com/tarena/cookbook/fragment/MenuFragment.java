53e3f0e28bfaf6b3f2ef0d745e83dcc18a5de4ef
ltpAndroid <511023726@qq.com>
2017-12-10 16:05:12 +0800

    IndexView

--- app/src/main/java/com/tarena/cookbook/fragment/MenuFragment.java
+++ app/src/main/java/com/tarena/cookbook/fragment/MenuFragment.java
@@ -5,8 +5,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.tarena.cookbook.R;
+import com.tarena.cookbook.view.IndexView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/10/15 0015.
@@ -14,17 +21,33 @@ import com.tarena.cookbook.R;
 
 public class MenuFragment extends BaseFragment {
 
+    @BindView(R.id.tv_classify_title)
+    TextView tvTitle;
+    @BindView(R.id.lv_classify)
+    ListView lvClassify;
+    @BindView(R.id.tv_classify_toast)
+    TextView tvToast;
+    @BindView(R.id.index_view)
+    IndexView indexView;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        contentView = inflater.inflate(R.layout.fragment_menu,container,false);
+        contentView = inflater.inflate(R.layout.fragment_menu, container, false);
         initialUI();
+        unbinder = ButterKnife.bind(this, contentView);
         return contentView;
     }
 
     @Override
     protected void initialUI() {
-    actionBar = contentView.findViewById(R.id.acitonBar_menu);
-    initialActionBar(0,"菜谱大全");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
