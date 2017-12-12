098eb58b6e0947e7ef6f7783bf0a18e94a57f690
ltpAndroid <511023726@qq.com>
2017-12-12 20:20:17 +0800

    Cookery-2017.12.12

--- app/src/main/java/com/tarena/cookbook/activity/ShowCookeryActivity.java
+++ app/src/main/java/com/tarena/cookbook/activity/ShowCookeryActivity.java
@@ -1,15 +1,49 @@
 package com.tarena.cookbook.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.tarena.cookbook.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ShowCookeryActivity extends AppCompatActivity {
+
+    @BindView(R.id.cookery_back)
+    ImageView ivBack;
+    @BindView(R.id.ll_search)
+    LinearLayout llSearch;
+    @BindView(R.id.tv_search)
+    TextView tvSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_cookery);
+        ButterKnife.bind(this);
+
+        Bundle bundle = getIntent().getExtras();
+        String search = bundle.getString("title");
+        tvSearch.setText(search);
+    }
+
+    @OnClick({R.id.cookery_back, R.id.ll_search})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.cookery_back:
+                finish();
+                break;
+            case R.id.ll_search:
+                Intent intent = new Intent(ShowCookeryActivity.this, SearchActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
